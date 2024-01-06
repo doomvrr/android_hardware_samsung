YaAlex3/hardware_samsung

fingerprint: Reset circle/brightness after enroll * Also reset if cancelled
Signed-off-by: Alexander <yaalex@yaalex.tk>

hidl/fingerprint/BiometricsFingerprint.cpp
@@ -276,6 +276,10 @@ Return<RequestStatus> BiometricsFingerprint::enroll(const hidl_array<uint8_t, 69
 }
 
 Return<RequestStatus> BiometricsFingerprint::postEnroll() {
+#ifdef HAS_OPTICAL_UDFPS
+    set("/sys/class/lcd/panel/fp_green_circle", 0);
+    set("/sys/class/lcd/panel/hbm_override", 0);
+#endif
     return ErrorFilter(ss_fingerprint_post_enroll());
 }
 
@@ -293,6 +297,11 @@ Return<RequestStatus> BiometricsFingerprint::cancel() {
         msg.data.error = FINGERPRINT_ERROR_CANCELED;
         notify(&msg);
     }
+#endif
+
+#ifdef HAS_OPTICAL_UDFPS
+    set("/sys/class/lcd/panel/fp_green_circle", 0);
+    set("/sys/class/lcd/panel/hbm_override", 0);
 #endif
 
     return ErrorFilter(ret);

doomvrr/android_hardware_samsung

hidl/fingerprint/BiometricsFingerprint.cpp
@@ -280,6 +280,10 @@ Return<RequestStatus> BiometricsFingerprint::enroll(const hidl_array<uint8_t, 69
 }
 
 Return<RequestStatus> BiometricsFingerprint::postEnroll() {
+#ifdef REQUEST_TOUCH_EVENT
+    set("/sys/class/lcd/panel/fp_green_circle", 0);
+    set("/sys/class/lcd/panel/hbm_override", 0);
+#endif
     return ErrorFilter(ss_fingerprint_post_enroll());
 }
 
@@ -297,6 +301,11 @@ Return<RequestStatus> BiometricsFingerprint::cancel() {
         msg.data.error = FINGERPRINT_ERROR_CANCELED;
         notify(&msg);
     }
+#endif
+
+#ifdef REQUEST_TOUCH_EVENT
+    set("/sys/class/lcd/panel/fp_green_circle", 0);
+    set("/sys/class/lcd/panel/hbm_override", 0);
 #endif
 
     return ErrorFilter(ret);